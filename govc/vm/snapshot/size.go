b8fa76c8d5d8b8ddfbacd35789c0b984bfa2a47f
Adam Chalkley <atc0005@users.noreply.github.com>
2021-02-04 05:59:55 -0600

    WIP: Begin trimming comments back

--- govc/vm/snapshot/size.go
+++ govc/vm/snapshot/size.go
@@ -1,13 +1,26 @@
+/*
+Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package snapshot
 
 import "github.com/vmware/govmomi/vim25/types"
 
-// returns vm.LayoutEx.Disk[].Chain[].FileKey[]
-// flattens file pairs into a single list
 func extractDiskLayoutFiles(diskLayoutList []types.VirtualMachineFileLayoutExDiskLayout) []int {
 	var result []int
 
-	// NOTE: layoutExDisk.Key is not referenced here.
 	for _, layoutExDisk := range diskLayoutList {
 		for _, link := range layoutExDisk.Chain {
 			for i := range link.FileKey { // diskDescriptor, diskExtent pairs (among others)
@@ -19,7 +32,6 @@ func extractDiskLayoutFiles(diskLayoutList []types.VirtualMachineFileLayoutExDis
 	return result
 }
 
-// removeKey (appears) to remove a given file key from the list
 func removeKey(l *[]int, key int) {
 	for i, k := range *l {
 		if k == key {
@@ -29,29 +41,7 @@ func removeKey(l *[]int, key int) {
 	}
 }
 
-/*
-	Questions:
-
-	- What is `parent`?
-	  - If `parent` is the parent snapshot, how do we know?
-	- How do we know if isCurrent should be false?
-	  - check against vm.Snapshot.CurrentSnapshot?
-*/
-func SnapshotSize(
-
-	info types.ManagedObjectReference,
-
-	// this function cannot be expected to process a snapshot tree. Instead,
-	// something else looks at the tree and calls this for each snapshot. This
-	// value would be the MOID for that snapshot.
-	parent *types.ManagedObjectReference,
-
-	// files that comprise this virtual machine
-	vmlayout *types.VirtualMachineFileLayoutEx,
-
-	// This can be determined by comparing info.CurrentSnapshot.Value?
-	isCurrent bool,
-) int {
+func SnapshotSize(info types.ManagedObjectReference, parent *types.ManagedObjectReference, vmlayout *types.VirtualMachineFileLayoutEx, isCurrent bool) int {
 
 	// flat list of diskDescriptor, diskExtent values
 	var fileKeyList []int
@@ -71,10 +61,9 @@ func SnapshotSize(
 		allSnapshotFiles = append(allSnapshotFiles, diskLayout...)
 
 		// if the layout MOID matches active snapshot MOID
-		// Q: Why match on this? Is this because the snapshot is growing?
 		if layout.Key.Value == info.Value {
 
-			// gather file keys if snapshot is current?
+			// gather file keys if snapshot is current
 			fileKeyList = append(fileKeyList, int(layout.DataKey)) // The .vmsn file
 			fileKeyList = append(fileKeyList, diskLayout...)       // The .vmdk files
 
@@ -85,20 +74,18 @@ func SnapshotSize(
 		}
 	}
 
-	// remove file keys associated with parent snapshot?
-	// Q: Why are the keys for these files removed?
+	// remove file keys associated with parent snapshot
 	for _, parentFile := range parentFiles {
 		removeKey(&fileKeyList, parentFile)
 	}
 
-	// remove all snapshot files from the list associated with the VM itself?
-	// leaving just the non-snapshot files? our baseline for comparison?
+	// remove all snapshot files from the list associated with the VM itself
 	for _, file := range allSnapshotFiles {
 		removeKey(&diskFiles, file)
 	}
 
-	// build a map of vm.LayoutEx.File.Key to vm.LayoutEx.File, presumably to
-	// make retrieving the size for a specific file easier later
+	// build a map of vm.LayoutEx.File.Key to vm.LayoutEx.File to make
+	// retrieving the size for a specific file easier later
 	fileKeyMap := make(map[int]types.VirtualMachineFileLayoutExFileInfo)
 	for _, file := range vmlayout.File {
 		fileKeyMap[int(file.Key)] = file
@@ -106,11 +93,6 @@ func SnapshotSize(
 
 	size := 0
 
-	// why are we excluding diskExtent files if there is a parent for the
-	// snapshot?
-	// Q: does `if parent != nil` filter to files that have a parent? If
-	// so, is the idea that snapshot files *have* a parent, but other
-	// files do not?
 	for _, fileKey := range fileKeyList {
 		file := fileKeyMap[fileKey]
 		if parent != nil ||
@@ -120,7 +102,8 @@ func SnapshotSize(
 		}
 	}
 
-	// if the snapshot *is* active, we add up all associated files?
+	// if the snapshot is active, add up all associated disk files not
+	// directly associated with the snapshot layout
 	if isCurrent {
 		for _, diskFile := range diskFiles {
 			file := fileKeyMap[diskFile]
